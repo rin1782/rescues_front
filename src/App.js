45b6a2069c364245190e36311c021ca991878b46
rin1782 <colaerin@yahoo.com>
2021-05-30 08:56:17 -0400

    fixed app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class App extends React.Component() {
+class App extends React.Component {
   
   componentDidMount(){
     fetch("http://localhost:3000/api/v1/rescues")
@@ -19,4 +19,4 @@ class App extends React.Component() {
   }
 }
 
-export default App;
+export default App

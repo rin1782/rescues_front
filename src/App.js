4993fc72261358f0c89944c2b56d28742ace4bbf
rin1782 <colaerin@yahoo.com>
2021-05-30 08:37:39 -0400

    ran npm install and modified app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
 class App extends React.Component() {
-  render(){
   
-  return (
-    <div className="App">
-      
-    </div>
-  )};
+  componentDidMount(){
+    fetch("http://localhost:3000/api/v1/rescues")
+    .then(r => r.json())
+    .then(data => {
+      console.log(data)
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        App
+      </div>
+    )
+  }
 }
 
 export default App;

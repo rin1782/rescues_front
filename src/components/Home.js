a9eb2e23acf066f38375ac446bcaef0c95f394ad
rin1782 <colaerin@yahoo.com>
2021-06-02 11:22:48 -0400

    added alt to img

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home = (props) => {
         <h1>Welcome to The Underdog Rescue Network</h1>
         <h4>Use the links above to search our current Rescue Centers and view their pets<br></br>
             or if you'd like, create your own!</h4>
-        <img src="https://i.pinimg.com/originals/1b/72/ab/1b72abba256fd82a71165d626807a875.png" width="500px" height="425px"/>
+        <img src="https://i.pinimg.com/originals/1b/72/ab/1b72abba256fd82a71165d626807a875.png" width="500px" height="425px" alt=""/>
         </div>
     )
 }

f7da8593b32d59a2cf40c0ef4240d5826f35cb12
rin1782 <colaerin@yahoo.com>
2021-06-04 15:30:05 -0400

    updating react bootstrap

--- src/components/Dogs.js
+++ src/components/Dogs.js
@@ -14,17 +14,16 @@ const Dogs = (props) => {
     }
 
     return(
-        <CardGroup>
+        <CardGroup style={{display: 'flex', width: '50%', border: 'solid', color: 'black'}}>
         
-        <Card.ImgOverlay src="https://www.petmd.com/sites/default/files/styles/article_image/public/petmd-puppy-weight.jpg?itok=IwMOwGSX" alt="puppy pic" />
             {props.dogs && props.dogs.map(dog => 
                 <Card key={dog.id} text={dog.name.toUpperCase()}>
                     <Card.Body>
+                    <Card.Img variant="top" src="https://www.petmd.com/sites/default/files/styles/article_image/public/petmd-puppy-weight.jpg?itok=IwMOwGSX" alt="puppy pic" />
                         <Card.Title>{dog.name}</Card.Title>
                         <Card.Text>{dog.description}</Card.Text>
-                        <Button variant="primary" onClick={() => handleAdopted(dog)}>Adopted</Button>
+                        <Button variant="dark" onClick={() => handleAdopted(dog)}>Adopted</Button>
                         </Card.Body>
-                    {/* <button className="adoptedBtn" onClick={() => handleAdopted(dog)}>Adopted</button> */}
                 </Card>
             )}
         

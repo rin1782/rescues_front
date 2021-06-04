4e6685a91a41e5c09a319df16ddb6b92a4e709b5
rin1782 <colaerin@yahoo.com>
2021-06-03 22:47:27 -0400

    starting a message board feature

---
+++ src/containers/MessagesContainer.js
@@ -0,0 +1,17 @@
+import React from 'react' 
+
+
+
+
+class MessagesContainer extends React.Component {
+
+    render() {
+        
+
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default MessagesContainer
